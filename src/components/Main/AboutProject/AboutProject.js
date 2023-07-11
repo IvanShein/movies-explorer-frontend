aaec41c4ca19aedea8fa2467e13e562d371dd62e
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-07-11 08:24:43 +0300

    fix: исправить отступы странице main в разрешении 1280

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -9,11 +9,11 @@ function AboutProject() {
 
             <div className="about-project__columns">
                 <div className="about-project__column">
-                    <h3 className="about-project__sublitle">Дипломный проект включал 5 этапов</h3>
+                    <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
                     <p className="about-project__text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
                 </div>
                 <div className="about-project__column">
-                    <h3 className="about-project__sublitle">На выполнение диплома ушло 5 недель</h3>
+                    <h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
                     <p className="about-project__text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
                 </div>
             </div>
