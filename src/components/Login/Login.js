c5fec0ab160e1febca5341da9a688c33a6445e3f
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-07-13 15:51:31 +0300

    feat: добавить компоненты Register и Login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css';
+import logo from "../../images/logo.svg";
 
 function Login() {
   return (
-    <section className="login">
-      <h1 className="login__title">Login</h1>
-      <p className="login__subtitle">тест</p>
-      <Link className='login__link' to='/'>На главную</Link>
-    </section>
+    <div className="login">
+      <header className="login__header link-decoration">
+        <Link to="/" className="login__header-link">
+          <img className="login__header-logo" src={logo} alt="Изображение логотипа сайта - Буква С в черном круге" />
+        </Link>
+        <h1 className="login__header-text">Рады видеть!</h1>
+      </header>
+      <main className="login__body">
+        <form className="login__form">
+          <span className="login__placeholder">E-mail</span>
+          <input
+            type="email"
+            className="login__input login__input_type_email"
+            required
+          />
+          <span className="login__error"></span>
+          <span className="login__placeholder">Пароль</span>
+          <input
+            type="password"
+            className="login__input login__input_type_password"
+            required
+          />
+          <span className="login__error"></span>
+          <div className="login__footer">
+            <button type="submit" className="login__footer-button">
+              Войти
+            </button>
+            <p className="login__footer-text">
+              Ещё не зарегистрированы? <Link to="/signup" className="login__footer-link link-decoration">
+                Регистрация
+              </Link>
+            </p>
+          </div>
+        </form>
+      </main>
+    </div>
   );
 }
 
