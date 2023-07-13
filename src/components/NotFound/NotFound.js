dc1dcfc5abca41fbe7e8b13c74533b615e671236
Ivan Shein <ivan-shein-yar@yandex.ru>
2023-07-13 16:12:46 +0300

    feat: добавить компонент NotFound

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 import './NotFound.css';
 
-function Login() {
+function NotFound() {
   return (
-    <section className="notFound">
-      <h1 className="notFound__title">404 - NotFound</h1>
-      <p className="notFound__subtitle">тест</p>
-      <Link className='notFound__link' to='/'>На главную</Link>
-    </section>
+    <main className="not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">Страница не найдена</p>
+      <Link to='/' className="not-found__link link-decoration">Назад</Link>
+    </main>
   );
 }
 
-export default Login;
+export default NotFound;
